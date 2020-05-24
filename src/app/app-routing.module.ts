4a7f7c4b20503b37be9d446c4a6e103c74ff0659
macbookpro <bratukhinmaks@gmail.com>
2020-05-24 16:40:42 +0200

    third commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,20 +4,13 @@ import {NgModule} from '@angular/core';
 import {MainComponent} from './main/main.component';
 import {FilmComponent} from './film/film.component';
 
-const PATH_BASE = 'https://hn.algolia.com/api/v1';
-const PATH_SEARCH = '/search';
-const PARAM_SEARCH = 'query=';
-const PARAM_PAGE = 'page=';
-const PARAM_HPP = 'hitsPerPage=';
-const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}&${PARAM_PAGE}`;
-
 
 
 const routes: Routes = [
     {
         path: '', component: SiteLayoutComponent, children: [
             {path:'main', component: MainComponent},
-            {path:'', component: FilmComponent}
+            {path:'main/:id', component: FilmComponent}
         ]
     }
 ];
